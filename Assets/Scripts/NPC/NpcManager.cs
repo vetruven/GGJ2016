3f9d6474ab9cf5a6ebb6046f7e7d0ccb6ba53946
Jonathan Goorin <jonathan@wesomegames.com>
2016-01-29 19:51:25 +0200

    no message

--- Assets/Scripts/NPC/NpcManager.cs
+++ Assets/Scripts/NPC/NpcManager.cs
@@ -6,16 +6,6 @@ public class NpcManager : MonoBehaviour
     [SerializeField] private GameObject _virginPrefab;
     [SerializeField] private Vector2 _generationRange = new Vector3(200,200,0);
 
-    private List<GameObject> _activeObjects;
-    private List<GameObject> _inactiveObjects;
-
-
-    void Awake()
-    {
-        _activeObjects = new List<GameObject>();
-        _inactiveObjects = new List<GameObject>();
-    }
-
     void Update()
     {
         if (Input.GetKey(KeyCode.P))
@@ -28,12 +18,7 @@ public class NpcManager : MonoBehaviour
     {
         Vector3 newPos = _generationRange;
         newPos.Scale(Random.onUnitSphere);
-
         var go = Instantiate(_virginPrefab, newPos, Quaternion.identity) as GameObject;
-
-        go.SetActive(true);
-        _activeObjects.Add(go);
-
         return go;
     }
 }
