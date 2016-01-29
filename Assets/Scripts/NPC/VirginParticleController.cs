96f5f4ad5468e2b1ec780d042958270eb8009c67
Jonathan Goorin <jonathan@wesomegames.com>
2016-01-29 19:24:46 +0200

    no message

---
+++ Assets/Scripts/NPC/VirginParticleController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VirginParticleController : MonoBehaviour
+{
+
+    public Transform TransformToFollow;
+    private ParticleSystem ps;
+
+    void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void FixedUpdate()
+    {
+        if (TransformToFollow != null)
+            transform.position = TransformToFollow.position;
+
+        if(!ps.isPlaying && !ps.IsAlive())
+            Destroy(gameObject);
+    }
+}
