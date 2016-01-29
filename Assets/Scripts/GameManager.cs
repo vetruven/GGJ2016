ca86ed6acda2e52c2fbbdf3384cef7d39148f184
Jonathan Goorin <jonathan@wesomegames.com>
2016-01-29 21:35:17 +0200

    no message

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            EventBus.StartGame.Dispatch();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            EventBus.EndGame.Dispatch();
+        }
+    }
+	
+}
--- Assets/Scripts/NPC/NpcManager.cs
+++ Assets/Scripts/NPC/NpcManager.cs
@@ -5,21 +5,53 @@ public class NpcManager : MonoBehaviour
 {
     [SerializeField] private GameObject _virginPrefab;
     [SerializeField] private Vector2 _generationRange = new Vector3(200,200,0);
+    [SerializeField] private float _creationInterval = 1; 
+
+    private bool _isCreating;
+    private float _creationTimer;
+
+    void Awake()
+    {
+        EventBus.StartGame.AddListener(StartGame);
+        EventBus.EndGame.AddListener(EndGame);
+    }
+
+    private void EndGame()
+    {
+        _isCreating = false;
+        foreach (var virgin in FindObjectsOfType<VirginController>())
+        {
+            Destroy(virgin.gameObject);
+        }
+    }
+
+    private void StartGame()
+    {
+        _isCreating = true;
+        for (int i = 0; i < 60; i++)
+        {
+            CreateVirgin();
+        }
+    }
+
+    void Start()
+    {
+    }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.P))
+        if (_isCreating && _creationTimer <= Time.time)
         {
-            GameObject go = GetInstance();
+            CreateVirgin();
+            _creationTimer = Time.time + _creationInterval;
         }
     }
 
 
-    private GameObject GetInstance()
+    private void CreateVirgin()
     {
         Vector3 newPos = _generationRange;
         newPos.Scale(Random.onUnitSphere);
         var go = Instantiate(_virginPrefab, newPos, Quaternion.identity) as GameObject;
-        return go;
     }
 }
