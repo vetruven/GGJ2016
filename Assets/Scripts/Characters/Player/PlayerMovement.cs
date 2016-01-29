28a76e739bc577246674f54da156184f823178e2
Paximillian <morzil@walla.com>
2016-01-29 15:24:22 +0200

    Movement Scripts

---
+++ Assets/Scripts/Characters/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[AddComponentMenu("Player/Player Movement")]
+public class PlayerMovement : CharacterMovement
+{
+    // Update is called once per frame
+    protected override void Update()
+    {
+        InputUpdate();
+
+        base.Update();
+    }
+
+    private void InputUpdate()
+    {
+        Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+}
