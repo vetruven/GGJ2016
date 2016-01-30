21358c3445e691f4d676eea124742d56a4d42a45
Paximillian <morzil@walla.com>
2016-01-30 11:29:47 +0200

    Added skill events, demon prefabs wip.

--- Assets/Scripts/EventBus.cs
+++ Assets/Scripts/EventBus.cs
@@ -10,4 +10,11 @@ public class EventBus
     public static Signal VirginDied = new Signal();
     public static Signal DemonAngry = new Signal();
     public static Signal EndGame = new Signal();
+    public static Signal BeaconActivated = new Signal();
+    public static Signal BeaconDeactivated = new Signal();
+    public static Signal GrapplerActivated = new Signal();
+    public static Signal GrapplerDeactivated = new Signal();
+    public static Signal IllusionActivated = new Signal();
+    public static Signal IllusionDeactivated = new Signal();
+    public static Signal SprintActivated = new Signal();
 }
