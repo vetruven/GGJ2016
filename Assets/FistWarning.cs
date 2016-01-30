1c60bf4a161a254ce9fb59c78de91caede66883d
Alexei Babin <alexei@moonactive.com>
2016-01-30 16:28:21 +0200

    Added fist warning

---
+++ Assets/FistWarning.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FistWarning : MonoBehaviour {
+
+	public static FistWarning Instance;
+
+	public Image notifier;
+	public GameObject container;
+	float currentSeconds;
+	float nextInterval;
+
+
+	// Use this for initialization
+	void Start () {
+		Instance = this;
+	}
+
+	public void ResetTimer(float seconds) {
+		nextInterval = seconds;
+		container.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(nextInterval == 0f)
+			return;
+
+		currentSeconds += Time.deltaTime;
+		notifier.fillAmount = currentSeconds / nextInterval;
+
+		if(currentSeconds > nextInterval)
+		{
+			nextInterval = 0;
+			currentSeconds = 0;
+			notifier.fillAmount = 0;
+			container.SetActive(false);
+		}
+	}
+}
