c4886b2ea82b7209f1e620c2469e467277824f72
Daniel Gelbshtein <daniel.kantorov@gmail.com>
2016-01-30 11:30:37 +0200

    AngerBar changes and DemonHand prefab fix

--- Assets/Scripts/AngerBar.cs
+++ Assets/Scripts/AngerBar.cs
@@ -5,73 +5,130 @@ using System.Collections;
 public class AngerBar : MonoBehaviour
 {
 
-    public Scrollbar angerBar;
-    public Color flashColor;
+    public Image angerBar;
 
     public float angryDrop;
     public float normalDrop;
 
-    public float angerTickSeconds;
-    public float currentAnger = 1f;
+    public float flashYellowThreshold = 0.33f;
+    public float flashRedThreshold = 0.66f;
+
+    public float currentAngerlevel = 1f;
     public float demonAngryEverySeconds = 2f;
+
+    public float virginEatingCost = 0.005f;
 
     private bool _gameOver = false;
     private float _deamonAngerSum = 0;
+    private Color _flashColor;
+    private Color _normalColor;
 
     void Start()
     {
+        _normalColor = angerBar.color;
         RegisterHandlers();
-        StartCoroutine("AngerManagement");
+        //StartCoroutine("AngerManagement");
     }
 
     void RegisterHandlers()
     {
         EventBus.StartLevel.AddListener(() =>
         {
-            StartCoroutine("AngerManagement");
         });
 
         EventBus.FinishLevel.AddListener(() =>
         {
             _gameOver = true;
         });
+
+        EventBus.UpdateBar.AddListener(() =>
+        {
+            AngerBarUpdate();
+        });
+
+        EventBus.VirginDied.AddListener(() => {
+            VirginEaten();
+        });
     }
 
-    IEnumerator AngerManagement()
+    void AngerBarUpdate()
     {
-        float angerDrop;
-        while (!_gameOver)
+        float drop;
+
+        if (_deamonAngerSum < demonAngryEverySeconds)
         {
-            yield return new WaitForSeconds(angerTickSeconds);
-            if (_deamonAngerSum < demonAngryEverySeconds)
+            drop = normalDrop;
+            _deamonAngerSum += Time.deltaTime;
+            if (_deamonAngerSum > _deamonAngerSum * flashYellowThreshold)
             {
-                angerDrop = normalDrop;
-                _deamonAngerSum += angerTickSeconds;
+                if (_deamonAngerSum > _deamonAngerSum * flashRedThreshold)
+                {
+                    FlashRed();
+                }
+                else
+                {
+                    FlashYellow();
+                }
             }
             else
             {
-                _deamonAngerSum = 0;
-                Debug.Log("sending demon angry");
-                EventBus.DemonAngry.Dispatch();
-                angerDrop= angryDrop;
+                ClearFlash();
             }
-            currentAnger -= angerDrop;
-            angerBar.size = currentAnger;
         }
+        else
+        {
+            _deamonAngerSum = 0;
+            EventBus.DemonAngry.Dispatch();
+            drop = angryDrop;
+        }
+        currentAngerlevel -= drop;
+        angerBar.fillAmount = currentAngerlevel;
     }
 
+    void FlashYellow()
+    {
+        StopCoroutine("BarFlashing");
+        _flashColor = Color.yellow;
+        StartCoroutine("BarFlashing", 0.5);
+    }
+
+    void FlashRed()
+    {
+        StopCoroutine("BarFlashing");
+        _flashColor = Color.red;
+        StartCoroutine("BarFlashing", 0.2);
+    }
+
+    void ClearFlash()
+    {
+        StopCoroutine("BarFlashing");
+        angerBar.color = _normalColor;
+    }
+
+    IEnumerator BarFlashing(float timeToFlash)
+    {
+        Color oldColor = angerBar.color;
+        while (true)
+        {
+            if (angerBar.color == _flashColor)
+            {
+                angerBar.color = oldColor;
+            }
+            else
+            {
+                angerBar.color = _flashColor;
+            }
+            yield return new WaitForSeconds(timeToFlash);
+        }
+    }
 
     public bool IsAngerBarEnded()
     {
-        return currentAnger == 0;
+        return currentAngerlevel == 0;
     }
 
-    public void VirginEaten(float virginWeight)
+    void VirginEaten()
     {
-        currentAnger += virginWeight;
-        if (currentAnger > 1)
-        {
-            currentAnger = 1;
-        }
+        currentAngerlevel += virginEatingCost;
     }
 }
