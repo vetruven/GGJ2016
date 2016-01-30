ea4eaec7a8e0324dd13a6f1c82937f8ad8b9d88c
Paximillian <morzil@walla.com>
2016-01-30 14:41:14 +0200

    Conjurer energy consumption fix.

--- Assets/Scripts/Characters/Player/Demon Classes/ConjurerDemon.cs
+++ Assets/Scripts/Characters/Player/Demon Classes/ConjurerDemon.cs
@@ -58,8 +58,9 @@ public class ConjurerDemon : BaseDemon
 
                 GameObject instantiatedObject = GameObject.Instantiate(m_DemonProxy.gameObject, transform.position, Quaternion.identity) as GameObject;
                 DemonProxy proxy = instantiatedObject.GetComponent<DemonProxy>();
-                proxy.Init(this, m_EnergyReplenishmentRate, m_AbilityCostPerSecond);
                 m_PlacedProxies.Enqueue(proxy);
+
+                StartCoroutine(depleteEnergy(m_AbilityCostPerSecond));
 
                 DisableEnergyReplenishment();
 
